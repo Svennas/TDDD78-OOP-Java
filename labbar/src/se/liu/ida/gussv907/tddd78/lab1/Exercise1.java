4c0ee911013b3166145d75242c2c279ce6af54cb
Gustav Svennas <gustavsvennas@gmail.com>
2017-02-14 12:51:37 +0100

    initia commit

---
+++ labbar/src/se/liu/ida/gussv907/tddd78/lab1/Exercise1.java
@@ -0,0 +1,10 @@
+package se.liu.ida.gussv907.tddd78.lab1;
+
+/**
+ * Created by svennas on 1/19/17.
+ */
+public class Exercise1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ labbar/src/se/liu/ida/gussv907/tddd78/lab3/DiagramComponent.java
@@ -0,0 +1,29 @@
+package se.liu.ida.gussv907.tddd78.lab3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+/**
+ * Created by svennas on 2/2/17.
+ */
+public class DiagramComponent extends JComponent {
+    @Override protected void paintComponent(final Graphics g) {
+        super.paintComponent(g);
+
+        for(Shape shape : shapes) {
+            shape.draw(g);
+        }
+    }
+
+    private List<Shape> shapes;
+
+    public DiagramComponent() {
+        shapes = new ArrayList<>();
+    }
+
+    public void addShape(Shape s) {
+        shapes.add(s);
+    }
+}
